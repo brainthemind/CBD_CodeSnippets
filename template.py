8f8483da2e5929bee9379e31ac3350b773c3ff33
Sophie Herbst <ksherbst@gmail.com>
2020-12-17 08:45:55 +0100

    initialize with template

---
+++ template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is an example for a code snippet, shared in the Cognition and
+Brain Dynamics Team.
+
+This is what it does:
+
+Author:
+
+Date:
+"""
